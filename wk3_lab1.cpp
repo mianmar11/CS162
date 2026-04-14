0965d7bf9bd4c0648c1ac9a84528cc6b249aa150
mianmar11 <zawyeyaintnaing11@gmail.com>
2026-04-14 18:35:44 +0000

    Init code file

---
+++ wk3_lab1.cpp
@@ -0,0 +1,23 @@
+/*
+Zaw Ye Yaint Naing
+
+CS162 Spring - Mitch Priestley
+
+Purpose:
+
+Specfication:
+*/
+
+#include <iostream> 
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    string name; // since string is an object, name is an instnace of string object (OOP)
+
+    name = "Zaw Ye Yaint Naing";                    
+
+    return 0;
+}
