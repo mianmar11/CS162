5cacc0e9471b08cbdc2dce32070ccf1e8a132e7e
mianmar11 <zawyeyaintnaing11@gmail.com>
2026-04-15 05:49:48 +0000

    Finish lab

--- wk3_lab1.cpp
+++ wk3_lab1.cpp
@@ -15,13 +15,38 @@ Specfication:
 using namespace std;
 
 int main() {
-    string name("Giorno Gorvana"); // (since string is an object, name is an instnace of string object (OOP))
+    // string name("Giorno Gorvana"); // (since string is an object, name is an instnace of string object (OOP))
 
-    char city[11] = "Seattle"; // (C-string)
+    char city[11] = "Portland"; // (C-string)
 
-    name = "Zaw Ye Yaint Naing"; // (RHS is a character array which is a C string)
+    // name = "Zaw Ye Yaint Naing"; // (RHS is a character array which is a C string)
 
     cout << city << endl;
+
+    // city[0] = "Seattle";
+
+    cout << static_cast<int>(city[7]) << endl; // 'd' of Portland
+
+    cout << static_cast<int>(city[8]) << endl; // the null terminator after the 'd'
+
+    city[0] = 'Z';
+
+    for (int idx = 0; idx < strlen(city); idx++) {
+        city[idx] = toupper(city[idx]);
+    }
+
+    cout << city << endl;
+
+    city[8] = 'Y';
+    city[9] = 'Z';
+    city[10] = 'A';
+    city[11] = 'B';
+
+    cout << city << endl;
+
+    strcpy(city, "89");
+
+    cout << atoi(city) + 1 << endl;
 
     return 0;
 }
