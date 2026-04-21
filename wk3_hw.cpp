366c4afd1c2fb2e29de58d56f7b7f2a9bc6e71b1
mianmar11 <zawyeyaintnaing11@gmail.com>
2026-04-21 22:29:59 +0000

    added text renderingsystem

--- wk3_hw.cpp
+++ wk3_hw.cpp
@@ -1,11 +1,24 @@
+/*
+
+Credits: StackOverflow. cppreference.com
+*/
+
 #include <iostream>
 #include <string>
+#include <chrono>
+#include <thread>
 
 using namespace std;
+using namespace chrono_literals;
+using namespace this_thread;
+
+void DisplayTxt(string txt);        // Display text at a pace just like in actual games
 
 int main() {
     // Init Variable
-    string username;        // player name
+    string username;                // Player name
+    string locations;               // Locations of the game
+    int current_loc {};             // Current location that player is in
 
     // Welcome User
     cout << "Hello Stranger.." << endl;
@@ -14,7 +27,24 @@ int main() {
     cout << "Hello, " << username << "." << endl;
     cout << "\nWelcome to .{Silver Grit}." << endl;
 
-    // 
+    cout << "Hello stranger, you are.." << endl;
+    cout << "[ENTER To Continue]" << endl;
+    
+    char dialogue[] = "Aren't you one of those heros that fought the Mighty Evil days before?";
+
+    for (int idx=0; dialogue[idx] != '\0'; idx++) {
+        cout << dialogue[idx] << flush;
+        sleep_for(20ms);
+    }
+
+    cout  << locations[current_loc];
 
     return 0;
+}
+
+void DisplayTxt(char msg[], int speed=20) {
+    for (int idx=0; msg[idx] != '\0'; idx++) {
+        cout << msg[idx] << flush;
+        sleep_for(20ms);
+    }
 }
