7065bd3e787aa9065673e93388537236e826284e
mianmar11 <zawyeyaintnaing11@gmail.com>
2026-05-05 18:53:58 +0000

    Init wk6 lab files

---
+++ wk6_lab1.cpp
@@ -0,0 +1,23 @@
+/*
+Zaw Ye Yaint Naing 
+
+CS162 Spring - Mitch Priestley
+
+Classes
+
+Purpose: This prgram specifies, implements, and demostrates a Point class.
+
+Specification: This program creates a Point class to store (x, y) ordered 
+pairs, add them, scale them, measure their magnitudes, and output them. The 
+ckass includes a default constructor, parameterized constructor, and destructor.
+A friend function overloads the <<operator to  permit cout <<pointA <<endl;
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // end normally
+    return 0;
+}
