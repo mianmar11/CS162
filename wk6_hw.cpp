a01e7ddfb588838ace07f64310bd2f3a4ae469ac
Mianmar <zawyeyaintnaing11@gmail.com>
2026-05-12 00:55:55 -0700

    Init wk6 homework & make lecture note for wk7 lab1

---
+++ wk6_hw.cpp
@@ -0,0 +1,22 @@
+/*
+Zaw Ye Yaint Naing
+
+CS162 Spring - Mitch Priestley 
+
+Purpose:
+
+Specification: 
+
+Credits:
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Welcome to the Customer Database Program." << endl << endl; 
+
+
+    return 0;
+}
