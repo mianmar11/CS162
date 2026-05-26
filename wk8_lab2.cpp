e1ecb8900b438e1ac1933286861838e902bbc9c8
mianmar11 <zawyeyaintnaing11@gmail.com>
2026-05-26 18:57:07 +0000

    Finish week 8 lab

--- wk8_lab2.cpp
+++ wk8_lab2.cpp
@@ -31,16 +31,18 @@ class Firm {
             strncpy(company_name, name, 30);
             company_name[30] = '\n';
 
-            // strncpy(city, city_name, sizeof(city) - 1);
-            // city[21] = '\0';
+            strncpy(city, city_name, sizeof(city) - 1);
+            city[21] = '\0';
         }
+
         // Getters and setters 
-        char* get_city() {return city;};
+        const char* get_city() {return city;};
         void set_city(const char arg_city[]) {
             strncpy(city, arg_city, sizeof(city) - 1); 
             city[21] = '\0';
         };
-        void display() {cout << "Co. Name: " << company_name << endl << " City: " << city << endl;}
+        
+        virtual void display() {cout << right << setw(15) << "Co. Name: " << company_name << endl << right << setw(15) << "City: " << city << endl;}
 };
 
 // Client "is a" Firm
@@ -48,13 +50,14 @@ class Client: public Firm {
     protected:
     public:
         char region; // 'E' for east, 'W' for west 
-        Client(const char* name, const char* city_name, char reg): Firm(name, city_name), region {reg} {};
+        Client(const char* name, const char* city_name, char reg): 
+            Firm(name, city_name), region {reg} {}
 
     public:
         void display() {
             cout << "Client information: " << endl;
             Firm::display();
-            cout << "Regoin: " << region << endl;
+            cout << right << setw(15) << "Regoin: " << region << endl;
         }
 };
 
@@ -62,6 +65,8 @@ class Vendor: public Firm {
     protected:
     public: 
         int quality; // 1 to 10 rating
+        Vendor(const char* name, const char* city_name, int qual): 
+            Firm(name, city_name), quality {qual} {};
 };
 
 
@@ -72,6 +77,8 @@ int main() {
         {"Daimler", "Portland", 'W'},
         {"Patagonia", "Portland", 'W'}
     };
+    Firm* f {};
+    Vendor vendor {"Coca-Cola", "Atlanta", 2};
 
     char choice[31] {}; // stores user's choice of client by name
 
@@ -86,12 +93,22 @@ int main() {
 
     cout << "Your choice: ";
     cin >> setw(31) >> choice;
+    cout << endl;
 
     for (Client c: client)
         if (strncasecmp(c.company_name, choice, sizeof(choice) - 1) == 0)
+            c.display();
 
+    f = &client[2]; // f = new firm;
+    f->display();   // use -> if using pointer variable to a object (dereferences the object, then calls the function)
 
+    // cout << endl;
+    // (*f).display(); // dereferences the object then calls the function 
 
-        c.display();
+    cout << endl;
+    // vendor.display();
+    f = &vendor;
+    f->display();
+
     return 0;
 }
